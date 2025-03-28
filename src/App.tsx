db8e8246dda15f777cabcbdf42956a895cfb3b2f
Krishang <krishang.note@gmail.com>
2025-03-28 12:21:20 +0530

    update heading

--- src/App.tsx
+++ src/App.tsx
@@ -451,10 +451,15 @@ export default function Game2048() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
       <h1
-      className="mb-8 text-6xl font-extrabold text-yellow-400 drop-shadow-[4px_4px_0px_rgba(255,0,0,1)] md:drop-shadow-[6px_6px_0px_rgba(255,0,0,1)] uppercase tracking-wider transform rotate-[-2deg]"
+      className=" text-8xl font-extrabold text-yellow-400 drop-shadow-[4px_4px_0px_rgba(255,0,0,1)] md:drop-shadow-[8px_8px_0px_rgba(255,0,0,1)] uppercase tracking-wider transform rotate-[-2deg]"
       >
         2048
       </h1>
+      <h3
+      className="mb-8 text-4xl font-extrabold text-purple-600 drop-shadow-[1px_1px_0px_rgba(255,0,0,1)] md:drop-shadow-[2px_2px_0px_rgba(255,0,0,1)] tracking-wider transform rotate-[-2deg]"
+      >
+        on MONAD
+      </h3>
 
       <div className="flex items-center justify-between w-full max-w-md mb-4">
 
