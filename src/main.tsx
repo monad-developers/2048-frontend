d0727939b3ab8285451364d67602e994917a8939
Krishang <krishang.note@gmail.com>
2025-04-18 20:24:27 +0530

    test main App

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from './App.tsx'
-import App from "./AltApp";
+import App from "./App.tsx";
 import { monadTestnet } from "viem/chains";
 import { PrivyProvider } from "@privy-io/react-auth";
 import { Toaster } from "@/components/ui/sonner";
