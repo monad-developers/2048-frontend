0147baa35a767b93601c6d24f72d9b4c330a6645
Krishang <krishang.note@gmail.com>
2025-05-07 09:54:10 +0530

    nit: wait 150ms between moves

--- src/App.tsx
+++ src/App.tsx
@@ -267,7 +267,7 @@ export default function Game2048() {
                 }
 
                 // Resume moves
-                new Promise((resolve) => setTimeout(resolve, 20));
+                await new Promise((resolve) => setTimeout(resolve, 150));
                 setIsAnimating(false);
             }
         } catch (error) {
