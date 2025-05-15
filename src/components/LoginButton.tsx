28ffd00b8e0c144ac6aaed3259dcfab4f68cba3b
Krishang <62195808+nkrishang@users.noreply.github.com>
2025-05-15 13:40:37 +0530

    feat: fund on login (#19)
    * feat: fund player on login
    
    * fix: loading states

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -7,6 +7,9 @@ import { toast } from "sonner";
 import FunPurpleButton from "./FunPurpleButton";
 import { Button } from "./ui/button";
 import { Copy } from "lucide-react";
+import { publicClient } from "@/utils/client";
+import { formatEther, Hex } from "viem";
+import { post } from "@/utils/fetch";
 
 type LoginButtonProps = {
     resetGame: () => void;
@@ -18,6 +21,7 @@ export default function LoginButton({ resetGame }: LoginButtonProps) {
     const { user, authenticated } = usePrivy();
 
     const [loginLoading, setLoginLoading] = useState(false);
+    const [faucetLoading, setFaucetLoading] = useState(false);
 
     const handleLogin = async () => {
         setLoginLoading(true);
@@ -51,6 +55,63 @@ export default function LoginButton({ resetGame }: LoginButtonProps) {
         setAddress((privyUser as any).address);
     }, [user]);
 
+    const handleFaucetRequest = async () => {
+        if (!user) {
+            toast.error("Please log-in.");
+            return;
+        }
+
+        const [privyUser] = user.linkedAccounts.filter(
+            (account) =>
+                account.type === "wallet" &&
+                account.walletClientType === "privy"
+        );
+        if (!privyUser || !(privyUser as any).address) {
+            toast.error("Embedded wallet not found.");
+            return;
+        }
+        const privyUserAddress = (privyUser as any).address;
+
+        const balance = await publicClient.getBalance({
+            address: privyUserAddress as Hex,
+        });
+        if (parseFloat(formatEther(balance)) >= 0.5) {
+            toast.info("Player has enough MON to play.");
+            return;
+        }
+
+        setFaucetLoading(true);
+
+        try {
+            const response = await post({
+                url: import.meta.env.VITE_2048_FAUCET_URL,
+                params: {
+                    address: privyUserAddress,
+                },
+            });
+
+            const transactionHash = response.txHash;
+            console.log("Funded tx: ", transactionHash);
+
+            toast.success(`Player funded!`, {
+                description: `Funded player with 0.5 MON from faucet.`,
+            });
+        } catch (e) {
+            console.log((e as any).message);
+            console.log("Error fetching testnet MON: ", e);
+            toast.info(`You'll need MON to play this game.`, {
+                description: `Continue playing and try the in-game faucet or fund directly.`,
+            });
+        }
+
+        setFaucetLoading(false);
+    };
+
+    useEffect(() => {
+        if (!user) return;
+        handleFaucetRequest();
+    }, [user]);
+
     const copyToClipboard = async () => {
         if (address) {
             await navigator.clipboard.writeText(address);
@@ -66,7 +127,12 @@ export default function LoginButton({ resetGame }: LoginButtonProps) {
         <>
             {user && authenticated ? (
                 <div className="flex flex-col items-center">
-                    <FunPurpleButton text="New Game" onClick={resetGame} />
+                    <FunPurpleButton
+                        text="New Game"
+                        onClick={resetGame}
+                        loadingText="Funding player..."
+                        isLoading={faucetLoading}
+                    />
                     <Button
                         variant="ghost"
                         className="underline"
