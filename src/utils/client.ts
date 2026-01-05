eb3cb996214ef0ffffc1726e1999afabce97ff70
Marcus <9083891+marcuspang@users.noreply.github.com>
2026-01-05 18:53:43 +0800

    chore: remove vite rpc url in env

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,16 +1,9 @@
 import { createPublicClient, http } from "viem";
 import { monadTestnet } from "viem/chains";
 
-const environment = import.meta.env.VITE_APP_ENVIRONMENT;
-const rpc =
-	environment === "prod"
-		? import.meta.env.VITE_MONAD_RPC_URL! ||
-			monadTestnet.rpcUrls.default.http[0]
-		: monadTestnet.rpcUrls.default.http[0];
-
 export const publicClient = createPublicClient({
 	chain: monadTestnet,
-	transport: http(rpc),
+	transport: http(),
 });
 
 let cachedFees: { maxFeePerGas: bigint; maxPriorityFeePerGas: bigint } | null =
