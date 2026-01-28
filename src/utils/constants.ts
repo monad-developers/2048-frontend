5c008e0145f5057c946de3338682e7755a310292
Krishang <krishang@monad.foundation>
2026-01-28 18:50:42 +0530

    nit: update deployment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import type { Network } from "@/contexts/NetworkContext";
 
 export const GAME_CONTRACT_ADDRESS: Record<Network, `0x${string}`> = {
-	testnet: "0xC52d29f79b2552801e95C8Dc7646f59125009904",
-	mainnet: "0x653d2536D82FA9812337D91e60BC2b9Fc19864Db",
+  testnet: "0xC52d29f79b2552801e95C8Dc7646f59125009904",
+  mainnet: "0x53748668642735CDa45935716525E7DFbC8aAACC",
 };
